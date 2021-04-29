723b682b2587abd0851130c5f3213e338e5135ae
andrewapp <andrew.appuhamy@gmail.com>
2021-04-29 22:09:45 +1000

    task 1 complete

--- lab4/lab4.m
+++ lab4/lab4.m
@@ -4,6 +4,8 @@ imgin = imread('images/pills1.png');
 
 img = imbinarize(imgin);
 
+img(150,487) = 1;
+img(151,487) = 1;
 
 
 figure(1)
@@ -11,9 +13,21 @@ imshow(img)
 
 imwrite(img,'pills1_binary.png')
 
-se = strel([50,50]);
+se = strel('disk', 16,0);
 
+se2 = strel('disk', 8,0);
 
 imgero = imerode(img,se);
 figure(2)
 imshow(imgero)
+
+imwrite(imgero, 'pills1_eroded.png')
+
+imgdil = imdilate(imgero,se2);
+
+figure(3)
+imshow(imgdil)
+
+imwrite(imgdil, 'pills1_dilated.png')
+
+
--- lab4/morphFilter.m
+++ lab4/morphFilter.m
@@ -10,3 +10,23 @@ function imgOut = morphFilter(imgIn)
 %   imgOut - the filtered binary image
 
 % PLACE YOUR CODE HERE (SUBMIT THIS FILE WITH YOUR REPORT)
+
+imgIn = imread('images/thumbprint.png');
+
+
+se = strel('disk', 2,0);
+
+se2 = strel('disk', 1,0);
+
+imgOut = imerode(imgIn,se2);
+
+imgOut = imdilate(imgOut,se);
+
+
+
+
+figure(1)
+imshow(imgOut)
+
+figure(2)
+imshow(imgIn)
